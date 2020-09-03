bce03c7f8c3a1c7b59f47f00b6fa292bb127d737
Thomas <t.man.atl.2012@gmail.com>
2020-09-03 14:46:39 -0700

    -i flag

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 anime cli tool
 
 do anime tasks from the command line
+uses myanimelist.net for most information
 
 usage:
 anime [1+ flags] name
@@ -9,10 +10,8 @@ anime [1+ flags] name
 flags:
 -m print mal desc. to standard out
 -r recommend a series after this one
+-i print information from mal sidebar
 -d # get #st ep. (inprogress)
 -s # get season # (inprogress)
 
-the name can be shortened
-if it does not match exactly, the first match on mal is used
-
-Thomas is someone who likes Anime...
+the name can be shortened, the first match on mal is used
--- anime.py
+++ anime.py
@@ -5,6 +5,7 @@ import urllib.request
 
 mflag = False # add others later
 rflag = False
+iflag = False
 sname = ""
 
 for i in range(1,len(sys.argv)):
@@ -12,6 +13,7 @@ for i in range(1,len(sys.argv)):
         for c in sys.argv[i]:
             if c == "m": mflag = True
             if c == "r": rflag = True
+            if c == 'i': iflag = True
     else:
         sname += " " + sys.argv[i] # assumes that name is treated as seperate args for each word
     # not sure if anime -r "one plus words" would break it
@@ -43,16 +45,28 @@ page = urllib.request.urlopen("http://" + link).read()
 page = page.decode("utf-8")
 
 titlepat = re.compile('title-name.+</h1>')
-
 descpat = re.compile('itemprop.{2}description.+?</p>', re.DOTALL)
+infopat = re.compile('<h2>Information.+?Statistics', re.DOTALL)
 
 descm = re.search(descpat, page)
 titlem = re.search(titlepat, page)
+infom = re.search(infopat, page)
 
 if (descm is None):
     print("no matches were found the second time")
     exit()
 
+if iflag:
+    #print(page[infom.start():infom.end()])
+    interesting = re.sub('(<.+?>)', '', page[infom.start():infom.end()-10])
+    interesting = re.sub(' +', ' ', interesting)
+    interesting = re.sub('^\w+$', '', interesting)
+    interesting = re.sub(':\n', ':', interesting)
+    interesting = re.sub(r'([A-Z][a-z0-9]+)\1', r'\1', interesting) 
+    for l in interesting.split('\n'):
+        if (len(l) > 2):
+            print(re.sub('([A-Za-z0-9]+)\1','\1',l))
+    print()
 if mflag:
     print(page[titlem.start()+12 : titlem.end()-5])
     print(page[descm.start()+23 : descm.end()-4].replace("<br />", "")) #description
