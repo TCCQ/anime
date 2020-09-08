037d3cc25ac5036b87579aa1c86b14ced1dfa362
Thomas <t.man.atl.2012@gmail.com>
2020-09-08 16:20:50 -0700

    Documentation, wow I know

--- anime.py
+++ anime.py
@@ -15,62 +15,57 @@ for i in range(1,len(sys.argv)):
             if c == "r": rflag = True
             if c == 'i': iflag = True
     else:
-        sname += " " + sys.argv[i] # assumes that name is treated as seperate args for each word
-    # not sure if anime -r "one plus words" would break it
-    # probably shell would kill the quotes and pass it all as one but idk
-    # I guess it would still be fine
+        sname += " " + sys.argv[i]  # nervous about this, but seems to work
+                                    # combines remaining args to make multi word names
 
 sname = sname.strip() # remove leading space
 sname = sname.replace(" ", "_")
 
-
 sh = urllib.request.urlopen("http://myanimelist.net/anime.php?q="+sname+"&cat=anime").read()
+sh = sh.decode("utf-8") # get the search page
 
-sh = sh.decode("utf-8")
-
-# print(sh)
-
-pat = re.compile('myanimelist.net/anime/\d+/[a-zA-Z0-9_-]+')
-
-
-
+pat = re.compile('myanimelist.net/anime/\d+/[a-zA-Z0-9_-]+') # look for anime matches
 m = re.search(pat, sh)
+
 if (m is None):
-    print("no matches were found (this is probably our fault)")
+    print("no matches were found (this is probably our fault)") # catch searches w/ no match, probably won't happen
     exit()
 
-link = sh[m.start(): m.end()]
+link = sh[m.start(): m.end()] # snag url of first hit
   
 page = urllib.request.urlopen("http://" + link).read()
-page = page.decode("utf-8")
+page = page.decode("utf-8") # get html of page of anime we are about
+page = re.sub('&amp;', '&', page); # clean &s in html
 
-titlepat = re.compile('title-name.+</h1>')
-descpat = re.compile('itemprop.{2}description.+?</p>', re.DOTALL)
-infopat = re.compile('<h2>Information.+?Statistics', re.DOTALL)
-
-descm = re.search(descpat, page)
-titlem = re.search(titlepat, page)
-infom = re.search(infopat, page)
-
-if (descm is None):
-    print("no matches were found the second time")
-    exit()
-
-if iflag:
-    #print(page[infom.start():infom.end()])
-    interesting = re.sub('(<.+?>)', '', page[infom.start():infom.end()-10])
-    interesting = re.sub(' +', ' ', interesting)
-    interesting = re.sub('^\w+$', '', interesting)
-    interesting = re.sub(':\n', ':', interesting)
-    interesting = re.sub(r'([A-Z][a-z0-9]+)\1', r'\1', interesting) 
+if iflag: # all sidebar info 
+    infopat = re.compile('<h2>Information.+?Statistics', re.DOTALL)
+    infom = re.search(infopat, page)
+    interesting = re.sub('(<.+?>)', '', page[infom.start():infom.end()-10]) # remove tags, leave contents
+    interesting = re.sub(' +', ' ', interesting) # colapse spaces and tabs to 1 space
+    interesting = re.sub('^\w+$', '', interesting) # remove lines w/o spaces (hacky and should be cleaned up)
+    interesting = re.sub(':\n', ':', interesting) # make Title: \n name into one line. 
+    interesting = re.sub(r'([A-Z][a-z0-9]+)\1', r'\1', interesting) # remove duplicated text 
+    # (for some reason the genres are listed twice, so this changes FantasyFantasy to just Fantasy. Relies of capitalization of genres) 
     for l in interesting.split('\n'):
-        if (len(l) > 2):
-            print(re.sub('([A-Za-z0-9]+)\1','\1',l))
+        if (len(l) > 2): # removes random lines w/ " \n" on them. not sure why these exist
+            print(l)
     print()
-if mflag:
-    print(page[titlem.start()+12 : titlem.end()-5])
-    print(page[descm.start()+23 : descm.end()-4].replace("<br />", "")) #description
+    
+if mflag: # description of anime (main body of mal)
+    titlepat = re.compile('title-name.+</h1>') 
+    descpat = re.compile('itemprop.{2}description.+?</p>', re.DOTALL)
+    titlem = re.search(titlepat, page)    
+    descm = re.search(descpat, page)
+    print(page[titlem.start()+12 : titlem.end()-5]) # name of series
+    print(page[descm.start()+23 : descm.end()-4].replace("<br />", "")) # description
+    print()
 
 
+# thoughts: maybe we should add a title at the top of the info section, it doesn't have one. 
+# the user needs to be sure that they actually got the series they were looking for
+# which is not a guarantee given how wack mal search is
 
+# need to add the recommendation section
+
+# instead of downloading, maybe just print out where you could find it? idk man
 
